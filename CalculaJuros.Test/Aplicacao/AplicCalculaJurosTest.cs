93dcd66197dcb18496a71b714ae4b35b6b97a47e
Paula Barbosa Mattos <paulamat@gmail.com>
2021-01-31 17:51:20 -0300

    Teste unitário CalculaJuros

---
+++ CalculaJuros.Test/Aplicacao/AplicCalculaJurosTest.cs
@@ -0,0 +1,20 @@
+﻿using CalculaJuros.Aplicacao.CalculaJuros;
+using CalculaJuros.Aplicacao.CalculaJuros.DTOs;
+using CalculaJuros.Aplicacao.TaxaJuros;
+using Moq;
+using Xunit;
+
+namespace CalculaJuros.Test.Aplicacao
+{
+    public class AplicCalculaJurosTest
+    {
+        public async void CalculaValorTotalComJurosDeveRetornar105_1()
+        {
+            var mockAplicTaxaJuros = new Mock<IAplicTaxaJuros>();
+            mockAplicTaxaJuros.Setup(p => p.TaxaJurosAsync()).ReturnsAsync(0.01m);
+            var aplic = new AplicCalculaJuros(mockAplicTaxaJuros.Object);
+            var valorTotal = await aplic.CalculaJurosAsync(new CalculaJurosDTO() { ValorInicial = 100, Tempo = 1 });
+            Assert.Equal(105.1m, valorTotal);
+        }
+    }
+}
---
+++ CalculaJuros.Test/Controllers/CalculaJurosControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculaJuros.Test.Controllers
+{
+    public class CalculaJurosControllerTest
+    {
+        public async void CalculaValorTotalComJurosDeveRetornar105_1()
+        {
+
+        }
+    }
+}
